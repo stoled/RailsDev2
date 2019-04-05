2825c765c3727f6fa18b7d88ab44f2527ede315f
GrigoryCode <grigorycode@gmail.com>
2019-04-05 10:10:21 +0300

    First one

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,36 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery3
+//= require activestorage
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require toastr
+//= require turbolinks
+//= require_tree .
+
+/*global toastr*/
+toastr.options = {
+  "closeButton": false,
+  "debug": false,
+  "positionClass": "toast-top-right mt-5",
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "1000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+};
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  get 'pages/about'
+  root 'pages#about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
