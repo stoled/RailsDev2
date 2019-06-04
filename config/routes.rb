29cae3c2d7092859086bdb02a4a7838b727d8105
GrigoryCode <grigorycode@gmail.com>
2019-06-05 01:51:30 +0300

    Index page and footer partial

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   get 'pages/about'
-  root 'pages#about'
+  root 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
